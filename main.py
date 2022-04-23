9acde8d285f0c2dc7e73ffd0222966e284fa968c
robbie632 <rob.632@hotmail.co.uk>
2022-04-23 18:34:02 +0100

    GUI inherited from Interface

--- main.py
+++ main.py
@@ -83,38 +83,7 @@ class CommandLine(Interface):
         playsound(filepath)
 
 
-class GUI(Interface):
-    def __init__(self):
-        Interface.__init__(self)
 
-    def intro(self):
-        pass
-
-    def ask(self, message: str):
-        """ask the user a phrase"""
-        raise NotImplementedError
-
-    def correct(self):
-        """give feedback that input was correct"""
-        raise NotImplemented
-
-    def incorrect(self):
-        """give feedback that input was incorrect"""
-        raise NotImplementedError
-
-    def check_weights(self, weights: str):
-        """show user probability distribution being used to
-        control picking of next phrase to ask"""
-        raise NotImplementedError
-
-    def ask_load_weights(self) -> bool:
-        """
-        Ask user if they want ot resume using weights from previous session
-        """
-        raise NotImplementedError
-
-    def play_phrase(self, filepath: str):
-        raise NotImplementedError
 
 
 
